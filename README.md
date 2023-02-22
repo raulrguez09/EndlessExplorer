4242303b1401ae0535429651baa709c9bf518cf4
raulrguez09 <raulbarsa09@gmail.com>
2023-02-22 19:21:20 +0100

    update sem2

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ El proyecto incluye material extraído del proyecto público [Three.js](http://t
 Para más información sobre su uso y alcance consultar la [wiki](https://github.com/RobVivo/RobVivo.github.io/wiki/INSTRUCCIONES-B%C3%81SICAS)
 
 Para comprobar el funcionamiento del navegador con WebGL
-[cargue esta página](http://robvivo.github.io)
+[cargue esta página](http://raulrguez09.github.io)
---
+++ js/ejemplo.js
@@ -0,0 +1,185 @@
+/**
+ * EscenaAnimada.js
+ * 
+ * Seminario AGM. Escena basica en three.js con interacción y animacion: 
+ * Animacion coherente, GUI, picking, orbitacion
+ * 
+ * @author <rvivo@upv.es>, 2022
+ * 
+ */
+
+// Modulos necesarios
+import * as THREE from "../lib/three.module.js";
+import {GLTFLoader} from "../lib/GLTFLoader.module.js";
+import {OrbitControls} from "../lib/OrbitControls.module.js";
+import {TWEEN} from "../lib/tween.module.min.js";
+import {GUI} from "../lib/lil-gui.module.min.js";
+
+// Variables estandar
+let renderer, scene, camera;
+
+// Otras globales
+let cameraControls, effectController;
+let esferaCubo,cubo,esfera;
+let angulo = 0;
+
+// Acciones
+init();
+loadScene();
+setupGUI();
+render();
+
+function init()
+{
+    // Instanciar el motor de render
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    document.getElementById('container').appendChild( renderer.domElement );
+
+    // Instanciar el nodo raiz de la escena
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0.5,0.5,0.5);
+
+    // Instanciar la camara
+    camera= new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,100);
+    camera.position.set(0.5,2,7);
+    cameraControls = new OrbitControls( camera, renderer.domElement );
+    cameraControls.target.set(0,1,0);
+    camera.lookAt(0,1,0);
+
+    // Eventos
+    renderer.domElement.addEventListener('dblclick', animate );
+}
+
+function loadScene()
+{
+    // Material sencillo
+    const material = new THREE.MeshBasicMaterial({color:'yellow',wireframe:true});
+
+    // Suelo
+    const suelo = new THREE.Mesh( new THREE.PlaneGeometry(10,10, 10,10), material );
+    suelo.rotation.x = -Math.PI/2;
+    suelo.position.y = -0.2;
+    scene.add(suelo);
+
+    // Esfera y cubo
+    esfera = new THREE.Mesh( new THREE.SphereGeometry(1,20,20), material );
+    cubo = new THREE.Mesh( new THREE.BoxGeometry(2,2,2), material );
+    esfera.position.x = 1;
+    cubo.position.x = -1;
+
+    esferaCubo = new THREE.Object3D();
+    esferaCubo.add(esfera);
+    esferaCubo.add(cubo);
+    esferaCubo.position.y = 1.5;
+
+    scene.add(esferaCubo);
+
+    scene.add( new THREE.AxesHelper(3) );
+    cubo.add( new THREE.AxesHelper(1) );
+
+    // Modelos importados
+    const loader = new THREE.ObjectLoader();
+    loader.load('models/soldado/soldado.json', 
+    function (objeto)
+    {
+        const soldado = new THREE.Object3D();
+        soldado.add(objeto);
+        cubo.add(soldado);
+        soldado.position.y = 1;
+        soldado.name = 'soldado';
+    });
+
+   // Importar un modelo en gltf
+   const glloader = new GLTFLoader();
+
+   glloader.load( 'models/robota/scene.gltf', function ( gltf ) {
+       gltf.scene.position.y = 1;
+       gltf.scene.rotation.y = -Math.PI/2;
+       gltf.scene.name = 'robota';
+       esfera.add( gltf.scene );
+   
+   }, undefined, function ( error ) {
+   
+       console.error( error );
+   
+   } );
+
+
+}
+
+function setupGUI()
+{
+	// Definicion de los controles
+	effectController = {
+		mensaje: 'Soldado & Robota',
+		giroY: 0.0,
+		separacion: 0,
+		colorsuelo: "rgb(150,150,150)"
+	};
+
+	// Creacion interfaz
+	const gui = new GUI();
+
+	// Construccion del menu
+	const h = gui.addFolder("Control esferaCubo");
+	h.add(effectController, "mensaje").name("Aplicacion");
+	h.add(effectController, "giroY", -180.0, 180.0, 0.025).name("Giro en Y");
+	h.add(effectController, "separacion", { 'Ninguna': 0, 'Media': 2, 'Total': 5 }).name("Separacion");
+    h.addColor(effectController, "colorsuelo").name("Color alambres");
+
+}
+
+function animate(event)
+{
+    // Capturar y normalizar
+    let x= event.clientX;
+    let y = event.clientY;
+    x = ( x / window.innerWidth ) * 2 - 1;
+    y = -( y / window.innerHeight ) * 2 + 1;
+
+    // Construir el rayo y detectar la interseccion
+    const rayo = new THREE.Raycaster();
+    rayo.setFromCamera(new THREE.Vector2(x,y), camera);
+    const soldado = scene.getObjectByName('soldado');
+    const robot = scene.getObjectByName('robota');
+    let intersecciones = rayo.intersectObjects(soldado.children,true);
+
+    if( intersecciones.length > 0 ){
+        new TWEEN.Tween( soldado.position ).
+        to( {x:[0,0],y:[3,1],z:[0,0]}, 2000 ).
+        interpolation( TWEEN.Interpolation.Bezier ).
+        easing( TWEEN.Easing.Bounce.Out ).
+        start();
+    }
+
+    intersecciones = rayo.intersectObjects(robot.children,true);
+
+    if( intersecciones.length > 0 ){
+        new TWEEN.Tween( robot.rotation ).
+        to( {x:[0,0],y:[Math.PI,-Math.PI/2],z:[0,0]}, 5000 ).
+        interpolation( TWEEN.Interpolation.Linear ).
+        easing( TWEEN.Easing.Exponential.InOut ).
+        start();
+    }
+}
+
+function update()
+{
+    angulo += 0.01;
+    //esferaCubo.rotation.y = angulo;
+
+    // Lectura de controles en GUI (es mejor hacerlo con onChange)
+	cubo.position.set( -1-effectController.separacion/2, 0, 0 );
+	esfera.position.set( 1+effectController.separacion/2, 0, 0 );
+	cubo.material.setValues( { color: effectController.colorsuelo } );
+	esferaCubo.rotation.y = effectController.giroY * Math.PI/180;
+    TWEEN.update();
+}
+
+function render()
+{
+    requestAnimationFrame(render);
+    update();
+    renderer.render(scene,camera);
+}
